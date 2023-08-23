66fe7a2dda7d292d92e13330b96c762d99bbdb9c
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-08-23 09:15:38 +0000

    folder name changed

--- app/components/FeedbackItemPopupComment.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FeedbackItemPopupComment() {
-  return (
-    <div>FeedbackItemPopupComment</div>
-  )
-}
-
-export default FeedbackItemPopupComment
---
+++ app/components/FeedbackItemPopupComments.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeedbackItemPopupComments() {
+  return (
+    <div>FeedbackItemPopupComments</div>
+  )
+}
+
+export default FeedbackItemPopupComments
