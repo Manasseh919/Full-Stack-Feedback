d203c0d2f638dc69711a1c5653cd94581766b2dd
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-09-21 09:25:40 +0000

    saving feedbacks to databse through api

--- app/components/FeedbackFormPopup.js
+++ app/components/FeedbackFormPopup.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Popup from "./Popup";
 import Button from "./Button";
+import axios from "axios";
 
 function FeedbackFormPopup({ setShow }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handlCreatePostButtonClick(ev) {
+    ev.preventDefault()
+    axios.post("/api/feedback", { title, description }).then((res) => {
+      console.log(res);
+    });
+  }
+
   return (
     <Popup setShow={setShow} title={" Make a Suggestion"}>
       <form className="p-8">
@@ -11,15 +22,22 @@ function FeedbackFormPopup({ setShow }) {
           className="w-full border rounded-md "
           type="text"
           placeholder="A short descriptive title"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
         />
         <label className="mt-4 mb-1  text-slate-700 block">Details</label>
+
         <textarea
           className="w-full border p-2 rounded-md"
           placeholder="please include any details"
-        ></textarea>
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
         <div className="flex gap-2 mt-2 justify-end">
           <Button>Attach files</Button>
-          <Button primary>Create post</Button>
+          <Button primary onClick={handlCreatePostButtonClick}>
+            Create post
+          </Button>
         </div>
       </form>
     </Popup>
